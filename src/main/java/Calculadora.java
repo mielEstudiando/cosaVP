744bd736d934386b5cee06152f8d36f8c7f86a0d
miel <m.ferrada09@ufromail.cl>
2023-10-04 09:35:35 -0300

    Diagrama + codigo creado con este

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+	private int _n1;
+	private int _n2;
+
+	public Calculadora() {
+		throw new UnsupportedOperationException();
+	}
+
+	public Calculadora(int aNum_1, int aNum2) {
+		throw new UnsupportedOperationException();
+	}
+
+	public int sumar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int multiplicar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setN1(int aNum1) {
+		this._n1 = aNum1;
+	}
+
+	public void setN2(int aNum2) {
+		this._n2 = aNum2;
+	}
+}
---
+++ src/main/java/CarroCompra.java
@@ -0,0 +1,19 @@
+public class CarroCompra {
+	private int[][] _productos;
+
+	public void Carro_compra() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int calcularTotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	public int subtotal() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mostrarTotal() {
+		throw new UnsupportedOperationException();
+	}
+}
